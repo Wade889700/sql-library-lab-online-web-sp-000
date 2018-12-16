53315ea11ee591e01882899f14e2fe149708880c
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-16 09:27:21 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,17 +18,42 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species)
+  FROM characters
+  GROUP BY species
+  ORDER BY COUNT(species)
+  DESC
+  LIMIT 1
+  "
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+   FROM series
+   INNER JOIN authors ON authors.id = series.author_id
+   INNER JOIN subgenres ON subgenres.id = series.subgenre_id
+  "
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+  FROM series
+  INNER JOIN characters ON series.id = characters.series_id
+  WHERE species = 'human'
+  GROUP BY series.title
+  ORDER BY COUNT(series.title)
+  DESC
+  LIMIT
+  1
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(name)
+  FROM character_books
+  INNER JOIN characters ON characters.id = character_books.character_id
+  INNER JOIN books ON books.id = character_books.book_id
+  GROUP BY name
+  ORDER BY COUNT(name) DESC
+  "
 end
