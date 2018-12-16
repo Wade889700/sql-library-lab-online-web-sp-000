363a67cbee2234c3fbb1d62aa95812d881d5aa4f
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-16 09:22:04 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -49,5 +49,9 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, books.number 
+  FROM character_books
+  INNER JOIN characters ON characters.id = character_books.character_id
+  INNER JOIN books ON books.id = character_books.book_id 
+  "
 end
