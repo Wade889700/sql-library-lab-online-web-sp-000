4cb40a378eea3884f08ea3c83f55a66a0be12fa3
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-16 09:26:42 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -52,8 +52,8 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, books.number
   FROM character_books
   INNER JOIN characters ON characters.id = character_books.character_id
-  INNER JOIN books ON books.id = character_books.book_id 
-  GROUP BY name 
+  INNER JOIN books ON books.id = character_books.book_id
+  GROUP BY name
   ORDER BY COUNT(name) DESC  
   "
 end
