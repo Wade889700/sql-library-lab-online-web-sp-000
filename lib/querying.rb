ae5d7548d9cb4610069fddbc1a41e9cf0068ed44
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-16 09:15:07 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -40,6 +40,11 @@ def select_series_title_with_most_human_characters
   FROM series
   INNER JOIN characters ON series.id = characters.series_id
   WHERE species = 'human'
+  GROUP BY series.title
+  ORDER BY COUNT(series.title)
+  DESC
+  LIMIT
+  1
   "
 end
 
